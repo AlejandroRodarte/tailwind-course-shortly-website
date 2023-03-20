ea6a234cd2908ed88cafb52e7b643d865c6f74e0
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2023-03-20 15:12:01 -0600

    setup, config and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+    extend: {
+      colors: {
+        cyan: 'hsl(180, 66%, 49%)',
+        'cyan-light': 'hsl(180, 66%, 69%)',
+        'dark-violet': 'hsl(257, 27%, 26%)',
+        red: 'hsl(0, 87%, 67%)',
+        'grayish-violet': 'hsl(257, 7%, 63%)',
+        'very-dark-blue': 'hsl(255, 11%, 22%)',
+        'very-dark-violet': 'hsl(260, 8%, 14%) ',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      spacing: {
+        128: '32rem',
+      },
     },
   },
   plugins: [],
