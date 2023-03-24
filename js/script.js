b5a1923afcd879d24d2f07ed103b5cc4170f3b93
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2023-03-23 20:38:31 -0600

    mobile menu section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+const menuBtn = document.getElementById('menu-btn');
+const errorMsg = document.getElementById('error-msg');
 const linkForm = document.getElementById('link-form');
 const linkInput = document.getElementById('link-input');
-const errorMsg = document.getElementById('error-msg');
+const menu = document.getElementById('menu');
 
 const isURLValid = (str) => {
   const pattern = new RegExp(
@@ -34,4 +36,22 @@ const onLinkFormSubmit = (e) => {
   }
 };
 
+const onMenuBtnClick = () => {
+  menu.classList.toggle('hidden');
+  menu.classList.toggle('flex');
+  menuBtn.classList.toggle('open');
+};
+
+const resizeObserver = new ResizeObserver((entries) => {
+  const [bodyEntry] = entries;
+  if (bodyEntry.target.clientWidth >= 1024) {
+    menu.classList.add('hidden');
+    menu.classList.remove('flex');
+    menuBtn.classList.remove('open');
+  }
+});
+
+resizeObserver.observe(document.body);
+
 linkForm.addEventListener('submit', onLinkFormSubmit);
+menuBtn.addEventListener('click', onMenuBtnClick);
